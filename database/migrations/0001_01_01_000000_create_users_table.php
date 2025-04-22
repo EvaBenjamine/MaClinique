0580bcc60de766dbd4c0db7a27573aabe21c3bdf
Killmonger27 <landryouarma45@gmail.com>
2025-04-22 11:29:37 +0000

    creation des utilisateurs avec differents roles et authentification

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nom');
+            $table->string('prenom');
+            $table->string('telephone');
+            $table->string('adresse');
+            $table->string('role');
+            $table->string('matricule')->nullable();
+            $table->boolean('is_active')->default(false);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
